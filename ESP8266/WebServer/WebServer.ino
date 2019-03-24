0af89fc0764d2a018bcc57667295db057d567070
sma96 <alexma3312@gatech.edu>
2019-03-24 16:42:06 -0400

    Update ESP8266.

--- ESP8266/WebServer/WebServer.ino
+++ ESP8266/WebServer/WebServer.ino
@@ -3,51 +3,51 @@
 
 ESP8266WebServer server;
 uint8_t pin_led = 16;
-uint8_t pin_motor = 15;
+uint8_t pin_motor = 5;
 char* ssid = "OPPO Find X";
 char* password = "12345678";
 
 void toggleLED()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);  
-  digitalWrite(pin_motor, 1.5); 
-  delay(1000);
-  digitalWrite(pin_motor, -1.5); 
-  delay(1000);
-  server.send(204,"");
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(1000);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(1000);  
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(1000);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(1000);  
+  digitalWrite(pin_motor, HIGH); 
+//  delay(100);
+  server.send(204,"Message Receive");
 }
 
 
 void setup() {
   pinMode(pin_led, OUTPUT);
   pinMode(pin_motor, OUTPUT);
-//  WiFi.begin(ssid, password);
-//  Serial.begin(115200);
-//  while(WiFi.status()!=WL_CONNECTED)
-//  {
-//    Serial.print(".");
-//    delay(500);
-//  }
-//  Serial.println("");
-//  Serial.println("WiFi connected");
-//  Serial.println("IP address: ");
-//  Serial.println(WiFi.localIP());
-//
-//  server.on("/",[](){server.send(200,"text/plain","Hello World");});
-//  
-//  server.begin();
+  WiFi.begin(ssid, password);
+  Serial.begin(115200);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/",[](){server.send(200,"text/plain","Hello World");});
+  
+  server.begin();
   
 }
 
 void loop() {
-  digitalWrite(pin_led, 1.5); 
+  digitalWrite(pin_led, HIGH); 
   delay(1000);
-  digitalWrite(pin_led, -1.5); 
-  delay(1000);
-//  server.handleClient();
-//  server.on("/toggle",toggleLED);
+  server.handleClient();
+  server.on("/toggle",toggleLED);
 
 }
--- ESP8266/wifi_client.py
+++ ESP8266/wifi_client.py
@@ -4,5 +4,6 @@ import keyboard
 http = httplib2.Http()
 
 url = "http://192.168.43.195/toggle"
-response, content = http.request(url,"Get")
-print(response)
+while(1):
+    response, content = http.request(url,"Get")
+    print(response)
