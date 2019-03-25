b20f9e074f67b747811bfbaf39eb18b2f6f15417
sma96 <alexma3312@gatech.edu>
2019-03-24 23:44:04 -0400

    FInish sim2 align.

--- sfm/sim2.py
+++ sfm/sim2.py
@@ -1,10 +1,10 @@
 """This is a two dimensional Similarity Transformation module"""
+import math
 import unittest
 
 import gtsam
 import numpy as np
 from gtsam import Point2, Pose2, Rot2
-import math
 
 
 class Similarity2(object):
@@ -39,6 +39,86 @@ class Similarity2(object):
             self._R.rotate(pose.translation()).vector() + self._t.vector()
         return Pose2(R.theta(), Point2(translation))
 
+    def align(self, point_pairs):
+        """
+        Generate similarity transform with Point2 pairs.
+        This function is based on gtsam::Pose2::align() 
+        *************************************************************************
+        It finds the angle using a linear method:
+        q = Pose2::transform_from(p) = t + R*p
+        We need to remove the centroids from the data to find the rotation
+
+        q1 = R*p1+t     -> (q1-c_q) = R * (p1-c_p) 
+        c_q = R*c_p+t
+
+        using dp=[dpx;dpy] and q=[dqx;dqy] we have
+         |dqx|   |c  -s|     |dpx|     |dpx -dpy|     |c|
+         |   | = |     |  *  |   |  =  |        |  *  | | = H_i*cs
+         |dqy|   |s   c|     |dpy|     |dpy  dpx|     |s|
+        where the Hi are the 2*2 matrices. Then we will minimize the criterion
+        J = \sum_i norm(q_i - H_i * cs)
+        Taking the derivative with respect to cs and setting to zero we have
+        cs = (\sum_i H_i' * q_i)/(\sum H_i'*H_i)
+
+        H_i'*H_i = (\sum_i dpx_i^2+dpy_i^2) |1  0|
+                                            |    |
+                                            |0  1|
+
+        The hessian is diagonal and just divides by a constant.
+        i.e., cos ~ sum(dpx*dqx + dpy*dqy) and sin ~ sum(-dpy*dqx + dpx*dqy)
+
+        The scale is the square root of the determinant of the rotation matrix because the rotation matrix is an orthogonal matrix.
+
+        The translation is then found from the centroids
+        as they also satisfy cq = t + sR*cp, hence t = cq - sR*cp
+        """
+
+        n = len(point_pairs)
+        assert n >= 2  # we need at least two pairs
+
+        # calculate centroids
+        s_center = Point2(0, 0)
+        d_center = Point2(0, 0)
+        for point_pair in point_pairs:
+            s_center = Point2(
+                s_center.x() + point_pair[0].x(), s_center.y() + point_pair[0].y())
+            d_center = Point2(
+                d_center.x() + point_pair[1].x(), d_center.y() + point_pair[1].y())
+        f = 1.0/n
+        s_center = Point2(s_center.x()*f, s_center.y()*f)
+        d_center = Point2(d_center.x()*f, d_center.y()*f)
+
+        # calculate cos and sin
+        c = 0
+        s = 0
+        constant = 0
+        for point_pair in point_pairs:
+            s_d = Point2(point_pair[0].x()-s_center.x(),
+                         point_pair[0].y()-s_center.y())
+            d_d = Point2(point_pair[1].x()-d_center.x(),
+                         point_pair[1].y()-d_center.y())
+            c += s_d.x() * d_d.x() + s_d.y() * d_d.y()
+            s += -s_d.y() * d_d.x() + s_d.x() * d_d.y()
+            constant += s_d.x()*s_d.x() + s_d.y()*s_d.y()
+
+        c /= constant
+        s /= constant
+
+        # calculate angle, scale, and translation
+        theta = math.atan2(s, c)
+        R = Rot2.fromAngle(theta)
+        determinant = np.linalg.det(np.array([[c, -s], [s, c]]))
+        if(determinant >= 0):
+            scale = determinant**(1/2)
+        elif(determinant < 0):
+            scale = -(-determinant)**(1/2)
+
+        t = R.rotate(s_center)
+        t = Point2(d_center.x() - scale*t.x(), d_center.y() - scale*t.y())
+
+        self._R = R
+        self._t = t
+        self._s = scale
 
     def map_transform(self):
         return
@@ -89,13 +169,31 @@ class TestSimilarity2(unittest.TestCase):
         poses, points = self.s_map
         for i, point in enumerate(points):
             point = sim2.point(point)
-            print(point)
             self.assert_gtsam_equals(expected_points[i], point)
 
         for i, pose in enumerate(poses):
             pose = sim2.pose(pose)
-            print(pose)
             self.assert_gtsam_equals(expected_poses[i], pose)
+
+    def test_align(self):
+        """Test generating similarity transform with Point2 pairs."""
+        s_point1 = Point2(0.5, 0.5)
+        s_point2 = Point2(2, 0.5)
+
+        d_point1 = Point2(9, 11)
+        d_point2 = Point2(9, 14)
+
+        point_pairs = [[s_point1, d_point1], [s_point2, d_point2]]
+        sim2 = Similarity2()
+        sim2.align(point_pairs)
+
+        expected_R = Rot2(math.radians(90))
+        expected_s = 2
+        expected_t = Point2(10, 10)
+
+        expected_R.equals(sim2._R, 0.01)
+        self.assertEqual(sim2._s, expected_s)
+        self.assert_gtsam_equals(sim2._t, expected_t)
 
 
 if __name__ == "__main__":
