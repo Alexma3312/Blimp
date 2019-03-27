5461f6d54416e35dccff7a9cf8df09d769887d32
sma96 <alexma3312@gatech.edu>
2019-03-27 17:39:49 -0400

    Add trajectory in Map Object.

--- atrium_control/mapping_and_localization_data.py
+++ atrium_control/mapping_and_localization_data.py
@@ -15,7 +15,7 @@ class Features(object):
         descriptors - an N*M numpy array of M dimension descriptors, for Superpoint M = 256
     """
 
-    def __init__(self, key_points, descriptors):
+    def __init__(self, key_points=np.zeros((0,2)), descriptors=np.zeros((0,256))):
         self.key_points = key_points
         self.descriptors = descriptors
         self.key_point_list = key_points.tolist()
@@ -54,14 +54,6 @@ class Features(object):
         r2 = npt.assert_almost_equal(self.descriptors, Features.descriptors)
         return r2
 
-    
-class Trajectory(object):
-    def __init__(self):
-        self.trajectory = []
-    def get_length(self):
-        return len(self.trajectory)
-
-
 
 class Map(object):
     """
@@ -70,13 +62,15 @@ class Map(object):
     Args:
         points - an N*3 numpy array of N gtsam.Point3 objects
         descriptors - an N*M numpy array of M dimension descriptors, for Superpoint M = 256
+        trajectory - an N*6 numpy array of N gtsam.Pose3 objects
     """
 
-    def __init__(self, points, descriptors):
+    def __init__(self, points = np.zeros((0,3)), descriptors=np.zeros((0,256)), trajectory=np.zeros((6,0))):
         self.landmark_points = points
         self.descriptors = descriptors
         self.landmark_list = points.tolist()
         self.descriptor_list = descriptors.tolist()
+        self.trajectory = trajectory
 
     def get_length(self):
         assert len(self.landmark_list) == len(self.descriptor_list)
@@ -99,6 +93,12 @@ class Map(object):
         self.descriptor_list.append(descriptor)
         self.landmark_points = np.array(self.landmark_list)
         self.descriptors = np.array(self.descriptor_list)
+
+    def add_trajectory(self, trajectory):
+        self.trajectory = trajectory
+
+    def append_trajectory(self,pose):
+        self.trajectory.append(pose)
     
     def assert_almost_equal(self, Map):
         # This function is for unittest, to test whether the actual output and expected output are the same
