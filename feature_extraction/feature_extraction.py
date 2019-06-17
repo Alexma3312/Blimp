eceb53aaf83d6c6a693653a2fa240a975eef9bd2
Shicong Ma <alexma3312@gmail.com>
2019-06-17 14:51:37 -0400

    Add draw features for Superpoint.

--- feature_extraction/feature_extraction.py
+++ feature_extraction/feature_extraction.py
@@ -12,11 +12,28 @@ from SuperPointPretrainedNetwork.demo_superpoint import (PointTracker,
 
 # pylint: disable=no-member
 
+# Font parameters for visualizaton.
+font = cv2.FONT_HERSHEY_DUPLEX
+font_clr = (255, 255, 255)
+font_pt = (4, 12)
+font_sc = 0.4
+# Jet colormap for visualization.
+myjet = np.array([[0., 0., 0.5],
+                  [0., 0., 0.99910873],
+                  [0., 0.37843137, 1.],
+                  [0., 0.83333333, 1.],
+                  [0.30044276, 1., 0.66729918],
+                  [0.66729918, 1., 0.30044276],
+                  [1., 0.90123457, 0.],
+                  [1., 0.48002905, 0.],
+                  [0.99910873, 0.07334786, 0.],
+                  [0.5, 0., 0.]])
+
 
 class FeatureExtraction(object):
     """Save superpoint extracted features to files."""
 
-    def __init__(self, image_directory_path='feature_extraction/undistort_images/', image_extension='*.jpg', image_size=(640, 480), nn_thresh=0.7):
+    def __init__(self, image_directory_path='SuperPointPretrainedNetwork/feature_extraction/undistort_images/', image_extension='*.jpg', image_size=(640, 480), nn_thresh=0.7):
         self.basedir = image_directory_path
         self.img_extension = image_extension
         self.nn_thresh = nn_thresh
@@ -54,9 +71,9 @@ class FeatureExtraction(object):
           heatmap - HxW numpy heatmap in range [0,1] of point confidences.
         """
 
-        superpoints, descriptors, _ = self.fe.run(image)
+        superpoints, descriptors, heatmap = self.fe.run(image)
 
-        return superpoints[:2, ].T, descriptors.T
+        return superpoints[:2, ].T, descriptors.T, heatmap
 
     def get_image_paths(self):
         """Get all image paths within the directory."""
@@ -74,7 +91,8 @@ class FeatureExtraction(object):
         """Extract features for each image within the image path list"""
         for i, impath in enumerate(self.img_paths):
             grayim = self.read_image(impath)
-            keypoints, descriptors = self.superpoint_generator(grayim)
+            keypoints, descriptors, heatmap = self.superpoint_generator(grayim)
+            self.draw_features(keypoints, grayim, heatmap, i)
             self.save_to_file(keypoints, descriptors, i)
 
     def save_to_file(self, kp_data, desc_data, index):
@@ -86,14 +104,14 @@ class FeatureExtraction(object):
                     for i, point in enumerate(kp_data)]
         features = np.array(features)
 
-        dir_name = self.basedir+'features/'
-        if not os.path.exists(dir_name):
-            os.mkdir(dir_name)
-        np.savetxt(dir_name+self.leading_zero(index) +
+        dirName = self.basedir+'features/'
+        if not os.path.exists(dirName):
+            os.mkdir(dirName)
+        np.savetxt(dirName+self.leading_zero(index) +
                    '.key', features, fmt='%.4f')
 
         first_line = str(nrpoints)+' '+str(descriptor_length)+'\n'
-        with open(dir_name+self.leading_zero(index)+'.key', 'r+') as f:
+        with open(dirName+self.leading_zero(index)+'.key', 'r+') as f:
             content = f.read()
             f.seek(0, 0)
             f.write(first_line+content)
@@ -107,5 +125,40 @@ class FeatureExtraction(object):
             output[7-index_len+i] = index_string[i]
         return ''.join(output)
 
+    def draw_features(self, keypoints, img, heatmap, index):
+        """Draw feature images and heatmap images."""
+        # Extra output -- Show current point detections.
+        out1 = (np.dstack((img, img, img)) * 255.).astype('uint8')
+        for pt in keypoints:
+            pt1 = (int(round(pt[0])), int(round(pt[1])))
+            cv2.circle(out1, pt1, 1, (0, 255, 0), -1, lineType=16)
+            cv2.putText(out1, 'Raw Point Detections', font_pt,
+                        font, font_sc, font_clr, lineType=16)
 
+        # Extra output -- Show the point confidence heatmap.
+        if heatmap is not None:
+            min_conf = 0.001
+            heatmap[heatmap < min_conf] = min_conf
+            heatmap = -np.log(heatmap)
+            heatmap = (heatmap - heatmap.min()) / \
+                (heatmap.max() - heatmap.min() + .00001)
+            out2 = myjet[np.round(np.clip(heatmap*10, 0, 9)).astype('int'), :]
+            out2 = (out2*255).astype('uint8')
+        else:
+            out2 = np.zeros_like(out1)
+            cv2.putText(out2, 'Raw Point Confidences', font_pt,
+                        font, font_sc, font_clr, lineType=16)
 
+        out_dir_1 = self.basedir+'feature_image/'
+        if not os.path.exists(out_dir_1):
+            os.mkdir(out_dir_1)
+        out_file_1 = out_dir_1+'frame_%05d' % index+'.jpg'
+        print('Writing image to %s' % out_file_1)
+        cv2.imwrite(out_file_1, out1)
+
+        out_dir_2 = self.basedir+'heatmap/'
+        if not os.path.exists(out_dir_2):
+            os.mkdir(out_dir_2)
+        out_file_2 = out_dir_2+'heatmap_%05d' % index+'.jpg'
+        print('Writing image to %s' % out_file_2)
+        cv2.imwrite(out_file_2, out2)
