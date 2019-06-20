306a30cd0074773cd088f587399536fcfd961d23
Shicong Ma <alexma3312@gmail.com>
2019-06-20 10:53:33 -0400

    Add test library 4X8.

--- feature_matcher/shicong_05_dsf_Library_4x8.py
+++ feature_matcher/shicong_05_dsf_Library_4x8.py
@@ -8,9 +8,9 @@ import numpy as np
 
 import gtsam
 from feature_matcher.mapping_back_end_dsf import MappingBackEnd
-from gtsam import Cal3_S2, Point3, Pose3, Rot3  # pylint: disable=ungrouped-imports
+from gtsam import Cal3_S2  # pylint: disable=ungrouped-imports
 from utilities.plotting import plot_with_result
-import math
+from utilities.pose_estimate_generator import pose_estimate_generator
 
 
 def run():
@@ -18,18 +18,20 @@ def run():
     # Input images(undistorted) calibration
     calibration = Cal3_S2(
         fx=232.0542, fy=252.8620, s=0, u0=325.3452, v0=240.2912)
-    # Camera to world rotation
-    # wRc = Rot3(1, 0, 0, 0, 0, 1, 0, -1, 0)  # pylint: disable=invalid-name
-    wRc = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
-    delta = np.radians(45)
     # Create pose estimates
-    pose_estimates = [Pose3(Rot3(np.dot(wRc,
-                                        # cRc'
-                                        np.array([[math.cos(delta*i), 0, -math.sin(delta*i)],
-                                                  [0, 1, 0],
-                                                  [math.sin(delta*i), 0, math.cos(delta*i)]])
-                                        )), Point3(0, 0, 1.5)) for i in range(8)]
-    
+    theta = 45
+    delta_x = 1
+    delta_y = -0.5
+    delta_z = 1.2
+    rows = 2
+    cols = 2
+    angles = 8
+
+    prior1_delta = [0, -1, 1.2, 0]
+    prior2_delta = [1, -1, 1.2, 0]
+    pose_estimates = pose_estimate_generator(
+        theta, delta_x, delta_y, delta_z, prior1_delta, prior2_delta, rows, cols, angles)
+
     # Create measurement noise for bundle adjustment
     sigma = 1.0
     measurement_noise = gtsam.noiseModel_Isotropic.Sigma(2, sigma)
@@ -40,17 +42,22 @@ def run():
                                   translation_sigma, translation_sigma, translation_sigma])
     pose_prior_noise = gtsam.noiseModel_Diagonal.Sigmas(pose_noise_sigmas)
     # Create MappingBackEnd instance
-    data_directory = 'feature_matcher/library_data/bright/'
-    num_images = 8
+    data_directory = 'feature_matcher/library_data/library_4X8/undistort_images/features/'
+    num_images = 34
+    filter_bad_landmarks_enable = True
+    min_obersvation_number = 2
+    prob = 0.9
+    threshold = 3
+    backprojection_depth = 2
     back_end = MappingBackEnd(data_directory, num_images, calibration,
-                              pose_estimates, measurement_noise, pose_prior_noise, 2, 3)
+                              pose_estimates, measurement_noise, pose_prior_noise, filter_bad_landmarks_enable, min_obersvation_number, prob, threshold, backprojection_depth)
     # Bundle Adjustment
     tic_ba = time.time()
     sfm_result = back_end.bundle_adjustment()
     toc_ba = time.time()
     print('BA spents ', toc_ba-tic_ba, 's')
     # Plot Result
-    plot_with_result(sfm_result)
+    plot_with_result(sfm_result, 5, 5, 5, 0.5)
 
 
 if __name__ == "__main__":
