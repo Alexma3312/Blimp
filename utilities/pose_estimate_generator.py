fdd7521540b8e8cc96e314ce0eaacf5baf525f18
Shicong Ma <alexma3312@gmail.com>
2019-08-25 15:35:58 -0400

    Create rect grid poses.

--- utilities/pose_estimate_generator.py
+++ utilities/pose_estimate_generator.py
@@ -17,22 +17,25 @@ def pose_estimate_generator(theta, delta_x, delta_y, delta_z, prior1_delta, prio
             delta_z - int, height
             prior1_delta - list, [x,y,z,rotation angle]
             prior2_delta - list, [x,y,z,rotation angle]
-            rows - int, number of rows
-            cols - int, number of columns
+            rows - int, number of rows, along x axis
+            cols - int, number of columns, along y axis
             angles - int, number of angles
             wRw2 - numpy array, rotation of the pose estimate grid
+                np.array([[math.cos(theta), 0, -math.sin(theta)], [0, 1, 0], [math.sin(theta), 0, math.cos(theta)]])
     """
     # Camera to world rotation
     wRc = Rot3(1, 0, 0, 0, 0, 1, 0, -1, 0).matrix()
-    wRc = Rot3(np.dot(wRc, wRw2)).matrix()
+    wRc2 = Rot3(np.dot(wRw2, wRc)).matrix()
 
     def image_pose(i):
         y_idx = i // (cols*angles)
         x_idx = (i % (cols*angles))//angles
         angle_idx = (i % (cols*angles)) % angles
-
-        rotation = Rot3(np.dot(wRc, np.array([[math.cos(theta*angle_idx), 0, -math.sin(
-            theta*angle_idx)], [0, 1, 0], [math.sin(theta*angle_idx), 0, math.cos(theta*angle_idx)]])))
+        # Basic rotation along z axis
+        degree = math.radians(theta*angle_idx)
+        Rz = np.array([[math.cos(degree), -math.sin(degree), 0],
+                       [math.sin(degree), math.cos(degree), 0], [0, 0, 1]])
+        rotation = Rot3(np.dot(Rz, wRc2))
         translation = Point3(delta_x*x_idx, delta_y*y_idx, delta_z)
         image_pose = Pose3(rotation, translation)
         return image_pose
@@ -40,13 +43,67 @@ def pose_estimate_generator(theta, delta_x, delta_y, delta_z, prior1_delta, prio
     image_poses = [image_pose(i) for i in range(rows*cols*angles)]
 
     # prior1_delta = [x_delta, y_delta, z_delta, theta_delta]
-    prior_rot_1 = Rot3(np.dot(wRc, np.array([[math.cos(prior1_delta[3]), 0, -math.sin(
-        prior1_delta[3])], [0, 1, 0], [math.sin(prior1_delta[3]), 0, math.cos(prior1_delta[3])]])))
+    degree_1 = math.radians(prior1_delta[3])
+    prior_rot_1 = Rot3(np.dot(np.array([[math.cos(degree_1), -math.sin(degree_1), 0], [
+                       math.sin(degree_1), math.cos(degree_1), 0], [0, 0, 1]]), wRc))
     prior_1 = Pose3(prior_rot_1, Point3(
         prior1_delta[0], prior1_delta[1], prior1_delta[2]))
     # prior2_delta = [x_delta, y_delta, z_delta, theta_delta]
-    prior_rot_2 = Rot3(np.dot(wRc, np.array([[math.cos(prior2_delta[3]), 0, -math.sin(
-        prior2_delta[3])], [0, 1, 0], [math.sin(prior2_delta[3]), 0, math.cos(prior2_delta[3])]])))
+    degree_2 = math.radians(prior2_delta[3])
+    prior_rot_2 = Rot3(np.dot(np.array([[math.cos(degree_2), -math.sin(degree_2), 0], [
+                       math.sin(degree_2), math.cos(degree_2), 0], [0, 0, 1]]), wRc))
+    prior_2 = Pose3(prior_rot_2, Point3(
+        prior2_delta[0], prior2_delta[1], prior2_delta[2]))
+
+    image_poses[:0] = [prior_2]
+    image_poses[:0] = [prior_1]
+
+    return image_poses
+
+def pose_estimate_generator_rectangle(theta, delta_x, delta_y, delta_z, prior1_delta, prior2_delta, rows, cols, angles, wRw2=np.identity(3)):
+    """Generate pose estimates for mapping.
+        Parameters:
+            theta - int, angle of rotation
+            delta_x - int list, delta distance along x axis
+            delta_y - int list, delta distance along y axis
+            delta_z - int, height
+            prior1_delta - list, [x,y,z,rotation angle]
+            prior2_delta - list, [x,y,z,rotation angle]
+            rows - int, number of rows, along x axis
+            cols - int, number of columns, along y axis
+            angles - int, number of angles
+            wRw2 - numpy array, rotation of the pose estimate grid
+                np.array([[math.cos(theta), 0, -math.sin(theta)], [0, 1, 0], [math.sin(theta), 0, math.cos(theta)]])
+    """
+    # Camera to world rotation
+    wRc = Rot3(1, 0, 0, 0, 0, 1, 0, -1, 0).matrix()
+    wRc2 = Rot3(np.dot(wRw2, wRc)).matrix()
+
+    def image_pose(i):
+        y_idx = i // (cols*angles)
+        x_idx = (i % (cols*angles))//angles
+        angle_idx = (i % (cols*angles)) % angles
+        # Basic rotation along z axis
+        degree = math.radians(theta*angle_idx)
+        Rz = np.array([[math.cos(degree), -math.sin(degree), 0],
+                       [math.sin(degree), math.cos(degree), 0], [0, 0, 1]])
+        rotation = Rot3(np.dot(Rz, wRc2))
+        translation = Point3(delta_x[x_idx], delta_y[y_idx], delta_z)
+        image_pose = Pose3(rotation, translation)
+        return image_pose
+
+    image_poses = [image_pose(i) for i in range(rows*cols*angles)]
+
+    # prior1_delta = [x_delta, y_delta, z_delta, theta_delta]
+    degree_1 = math.radians(prior1_delta[3])
+    prior_rot_1 = Rot3(np.dot(np.array([[math.cos(degree_1), -math.sin(degree_1), 0], [
+                       math.sin(degree_1), math.cos(degree_1), 0], [0, 0, 1]]), wRc))
+    prior_1 = Pose3(prior_rot_1, Point3(
+        prior1_delta[0], prior1_delta[1], prior1_delta[2]))
+    # prior2_delta = [x_delta, y_delta, z_delta, theta_delta]
+    degree_2 = math.radians(prior2_delta[3])
+    prior_rot_2 = Rot3(np.dot(np.array([[math.cos(degree_2), -math.sin(degree_2), 0], [
+                       math.sin(degree_2), math.cos(degree_2), 0], [0, 0, 1]]), wRc))
     prior_2 = Pose3(prior_rot_2, Point3(
         prior2_delta[0], prior2_delta[1], prior2_delta[2]))
 
--- utilities/tests/test_pose_estimate_generator_temp.py
+++ utilities/tests/test_pose_estimate_generator_temp.py
@@ -14,7 +14,7 @@ from mpl_toolkits.mplot3d import Axes3D  # pylint: disable=unused-import
 import gtsam.utils.plot as gtsam_plot
 from gtsam import Point3, Pose3, Rot3
 from gtsam.utils.test_case import GtsamTestCase
-from utilities.pose_estimate_generator import pose_estimate_generator
+from utilities.pose_estimate_generator import pose_estimate_generator, pose_estimate_generator_rectangle
 
 
 class TestMapping(GtsamTestCase):
@@ -22,30 +22,24 @@ class TestMapping(GtsamTestCase):
 
     def test_pose_estimate_generator(self):
         """test pose estimate generator"""
-
-        # theta = 45
-        # delta_x = 1
-        # delta_y = 2
-        # delta_z = 3
-        # rows = 2
-        # cols = 2
-        # angles = 8
+        # Camera to world rotation
+        wRc = Rot3(1, 0, 0, 0, 0, 1, 0, -1, 0)  # pylint: disable=invalid-name
         theta = 45
-        delta_x = 1
-        delta_y = 2
-        delta_z = 3
-        rows = 2
-        cols = 2
-        angles = 4
-        degree = math.radians(45)
+        delta_x = [0,3.7592, 3.7592+5]
+        delta_y = [0,1.75895,1.75895*2,1.75895*3]
+        delta_z = 0.9652
+        rows = 4
+        cols = 3
+        angles = 1
+        degree = math.radians(theta)
         R = np.array([[math.cos(degree), -math.sin(degree), 0],
                        [math.sin(degree), math.cos(degree), 0], [0, 0, 1]])
 
-        prior1_delta = [5, 5, 5, 30]
-        prior2_delta = [-5, -5, -3, -90]
+        prior1_delta = [0, 0, delta_z, 0]
+        prior2_delta = [3.7592, 0, delta_z, 0]
 
-        actual_pose_estimates = pose_estimate_generator(
-            theta, delta_x, delta_y, delta_z, prior1_delta, prior2_delta, rows, cols, angles, R)
+        actual_pose_estimates = pose_estimate_generator_rectangle(
+            theta, delta_x, delta_y, delta_z, prior1_delta, prior2_delta, rows, cols, angles)
 
         # Declare an id for the figure
         figure_number = 0
