1464ba8cdc09a489c4c25444b6547a89f7ea9cf2
Shicong Ma <alexma3312@gmail.com>
2019-09-26 20:34:51 -0400

    Finish read essential data.

--- shonan_averaging/parser.py
+++ shonan_averaging/parser.py
@@ -1,6 +1,29 @@
+"""Parser."""
+import os
+
+import numpy as np
+
+from shonan_averaging.myconfig import (basedir, calibration_matrix,
+                                       number_images)
 
 
-def read_data():
-    """"""
-    # Return estimated poses
+def read_essential_data():
+    """Read Data from file
+    """
+    dir_name = basedir+'matches/'
+    file_name = dir_name+'essential_matrices.dat'
+
+    with open(file_name) as file:
+        lines = file.readlines()[2:]
+    data = [list(map(np.float,match.split())) for match in lines]
+    essential_dict = {(int(match[0]),int(match[1])):np.array(match[2:11]).reshape(3,3) for match in data}
+    
+    return essential_dict
+
+
+def decompose_essential():
+    read_essential_data()
     pass
+
+
+read_essential_data()
